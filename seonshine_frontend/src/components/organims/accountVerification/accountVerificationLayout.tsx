5d933110fa38e6b263944ea1d68fd8d03f091e98
Nguyễn Văn Thoại <127712437+thoaiIT@users.noreply.github.com>
2024-06-07 13:19:54 +0700

    Account Verification Common Component (#5)
    * create component
    
    * handle logic and validation
    
    * style
    
    * reponsive and set timer
    
    * define route
    
    * fix comment

---
+++ seonshine_frontend/src/components/organims/accountVerification/accountVerificationLayout.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from 'react';
+
+import { Stack, Typography } from '@mui/material';
+
+type AccountVerificationLayoutProps = {
+  title?: string;
+  description: string;
+  children: ReactNode;
+};
+
+const AccountVerificationLayout = ({ title, description, children }: AccountVerificationLayoutProps) => {
+  const renderDescription = description.split('. ').map((text, index, array) => (
+    <React.Fragment key={index}>
+      {`${text}${index < array.length - 1 ? '.' : ''}`}
+      <br />
+    </React.Fragment>
+  ));
+  return (
+    <Stack
+      alignItems="center"
+      justifyContent="center"
+      className="min-h-screen"
+    >
+      <Stack
+        direction="column"
+        gap="24px"
+        alignItems="center"
+        className="w-full p-24 bg-white rounded-lg shadow-md md:w-max xl:w-240 max-w-screen"
+      >
+        <Typography
+          variant="heading2"
+          component="h2"
+          className="text-center"
+        >
+          {title}
+        </Typography>
+        <Typography
+          variant="bodyS"
+          className="text-center min-w-max"
+        >
+          {renderDescription}
+        </Typography>
+        {children}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default AccountVerificationLayout;
