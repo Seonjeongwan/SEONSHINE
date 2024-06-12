f2d9e837405e2a7de2e2506c7049370413bd5c31
Pham Van Hao <pvhao.d14pm@gmail.com>
2024-06-12 15:45:48 +0700

    add node_module volume to docker compose dev file

--- seonshine_backend/db/redisClient.js
+++ seonshine_backend/db/redisClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: 'redis://localhost:6379'
+  url: 'redis://redis:6379'
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error :>> ', err));
