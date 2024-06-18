4f7c3fad91f975ef1259276044671b803be220b2
sgo1026781 <phulocnguyen1610@gmail.com>
2024-06-18 14:51:34 +0700

    Fe define search bar component

--- seonshine_frontend/src/pages/dashboard/index.tsx
+++ seonshine_frontend/src/pages/dashboard/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import SearchBar from '@/components/molecules/searchBar';
+
 import { paths } from '@/routes/paths';
 
 const Dashboard = () => {
+  const handleSearch = (field: string, query: string) => {
+    console.log(`Searching for ${query} in field ${field}`);
+  };
+  const options = [
+    { value: 'user_id', label: 'ID' },
+    { value: 'fullname', label: 'Fullname' },
+    { value: 'branch', label: 'Branch' },
+  ];
+  const defaultOption = options[0].value;
   return (
     <div>
+      <SearchBar
+        onSearch={handleSearch}
+        options={options}
+      />
       TestPage
       <Link to={paths.forgotPassword}>GO TO</Link>;
     </div>
