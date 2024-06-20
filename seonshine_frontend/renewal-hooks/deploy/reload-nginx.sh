0cf47df93b3643cb9a01dc18506e5f825653b82a
Pham Van Hao <pvhao.d14pm@gmail.com>
2024-06-20 17:43:14 +0700

    update reload nginx file

--- seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
+++ seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ -f /etc/nginx/conf.d/default.ssl.conf ]; then
+    mount | grep /etc/nginx/conf.d/default.conf
     cp /etc/nginx/conf.d/default.ssl.conf /etc/nginx/conf.d/default.conf
     nginx -s reload
 fi
