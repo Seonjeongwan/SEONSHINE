339400aacc969b14bf95d6eb3152ef657ee6942e
seon <sunnythek@gmail.com>
2024-06-21 10:36:27 +0700

    docker compose fix

--- seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
+++ seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
@@ -2,5 +2,5 @@
 if [ -f /etc/nginx/conf.d/default.ssl.conf ]; then
     # mount | grep /etc/nginx/conf.d/default.conf
     cp /etc/nginx/conf.d/default.ssl.conf /etc/nginx/conf.d/default.conf
-    docker-compose -f /path/to/docker-compose.prod.yml exec frontend nginx -s reload
+    nginx -s reload
 fi
