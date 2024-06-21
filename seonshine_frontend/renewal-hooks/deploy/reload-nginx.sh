e379e93ee0b28d83bba70938d84e82dedc1e386c
Pham Van Hao <pvhao.d14pm@gmail.com>
2024-06-21 13:10:12 +0700

    update cerbot command

--- seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
+++ seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f /etc/nginx/conf.d/default.ssl.conf ]; then
     cp /etc/nginx/conf.d/default.ssl.conf /etc/nginx/conf.d/default.conf
-    nginx -s reload
+    docker exec $(docker ps -qf "name=frontend") nginx -s reload
 fi
 
     # mount | grep /etc/nginx/conf.d/default.conf
