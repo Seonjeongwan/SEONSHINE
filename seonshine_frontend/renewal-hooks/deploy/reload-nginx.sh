f8d554a29a839e7d1ebc5e6b21ad3cd90d91c90a
Pham Van Hao <pvhao.d14pm@gmail.com>
2024-06-21 11:43:56 +0700

    update reload nginx file

--- seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
+++ seonshine_frontend/renewal-hooks/deploy/reload-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f /etc/nginx/conf.d/default.ssl.conf ]; then
     cp /etc/nginx/conf.d/default.ssl.conf /etc/nginx/conf.d/default.conf
-    docker exec $(docker ps -qf "name=frontend") nginx -s reload
+    nginx -s reload
 fi
 
     # mount | grep /etc/nginx/conf.d/default.conf
