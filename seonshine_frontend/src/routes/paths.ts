9c5f1ebe55da4b38a06a45880d037b5a67999398
Nguyễn Văn Thoại <127712437+thoaiIT@users.noreply.github.com>
2024-06-26 10:20:01 +0700

    Implement Restaurant Assignment (#23)
    * change route
    
    * implement assign restaurant
    
    * change api

--- seonshine_frontend/src/routes/paths.ts
+++ seonshine_frontend/src/routes/paths.ts
@@ -8,7 +8,7 @@ export const paths = {
   admin: '/admin',
   dashboard: '/dashboard',
   user: { index: 'user', detail: ':id', register: 'register', edit: ':id/edit' },
-  restaurant: { index: 'restaurant' },
+  restaurantAssignment: { index: 'restaurant-assign' },
   menu: { index: 'menu' },
   order: { index: 'order' },
 };
