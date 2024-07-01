60b66c5c2a932b5ec0ca511e25672b88eaf46975
Nguyễn Văn Thoại <127712437+thoaiIT@users.noreply.github.com>
2024-07-01 14:08:24 +0700

    integrate change password api (#33)

--- seonshine_frontend/src/apis/hooks/authApi.hook.ts
+++ seonshine_frontend/src/apis/hooks/authApi.hook.ts
@@ -1,13 +1,61 @@
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 import { LoginSchemaType } from '@/pages/login/schemas';
 
-import { login, LoginResponseType } from '../auth';
+import {
+  LoginResponseType,
+  ResetPasswordPayloadType,
+  ResetPasswordResponseType,
+  SendOTPPayloadType,
+  SendOTPResponseType,
+  VerifyOTPPayloadType,
+  VerifyOTPResponseType,
+} from '@/types/auth';
+import { IErrorResponse } from '@/types/common';
 
-export const useLoginApi = (): UseMutationResult<LoginResponseType, unknown, LoginSchemaType> => {
+import { login, reSendOTP, resetPassword, sendOTP, verifyOTP } from '../auth';
+
+export const useLoginApi = (): UseMutationResult<LoginResponseType, IErrorResponse, LoginSchemaType> => {
   return useMutation({
     mutationFn: async (payload) => {
-      return login(payload.employeeId, payload.password);
+      return await login(payload.employeeId, payload.password);
+    },
+  });
+};
+
+export const useSendOTPApi = (): UseMutationResult<SendOTPResponseType, IErrorResponse, SendOTPPayloadType> => {
+  return useMutation({
+    mutationFn: async (payload) => {
+      return await sendOTP(payload);
+    },
+  });
+};
+
+export const useReSendOTPApi = (): UseMutationResult<SendOTPResponseType, IErrorResponse, SendOTPPayloadType> => {
+  return useMutation({
+    mutationFn: async (payload) => {
+      return await reSendOTP(payload);
+    },
+  });
+};
+
+export const useVerifyOTPApi = (): UseMutationResult<VerifyOTPResponseType, IErrorResponse, VerifyOTPPayloadType> => {
+  return useMutation({
+    mutationFn: async (payload) => {
+      return await verifyOTP(payload);
+    },
+  });
+};
+
+export const useResetPasswordApi = (): UseMutationResult<
+  ResetPasswordResponseType,
+  IErrorResponse,
+  ResetPasswordPayloadType
+> => {
+  return useMutation({
+    mutationFn: async (payload) => {
+      return await resetPassword(payload);
     },
   });
 };
