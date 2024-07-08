9c2696ea9033a1a569e8821edd124c890b283299
LocPhuNguyen <131144452+LocPhuNguyen@users.noreply.github.com>
2024-07-08 08:38:07 +0700

    Fe improve signup layout (#41)
    * improve signup layout
    
    * improve signup layout
    
    * improve checklist uat
    
    * improve sign-up responsive on mobile

--- seonshine_frontend/src/components/molecules/formEntity/label.tsx
+++ seonshine_frontend/src/components/molecules/formEntity/label.tsx
@@ -4,7 +4,16 @@ type FormLabelType = {
 };
 export const FormLabel = ({ title, required }: FormLabelType): JSX.Element => {
   return (
-    <label className="font-medium">
+    <label
+      className="font-medium"
+      style={{
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        display: 'inline-block',
+        maxWidth: '100%',
+      }}
+    >
       {title}&nbsp;
       {required && <span className="text-red-500">*</span>}
     </label>
