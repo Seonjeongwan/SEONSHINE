7890526e29f04633b926bc42f3a825b274391151
thoaiIT <nvthoai2k1@gmail.com>
2024-07-09 17:30:23 +0700

    fix comments

--- seonshine_backend/middleware/validation/restaurantValidate.js
+++ seonshine_backend/middleware/validation/restaurantValidate.js
@@ -1,15 +1,16 @@
+import { weekdays } from '../../constants/common.js';
 import { httpStatusCodes } from '../../constants/http.js';
 
 export const validateAssignRestaurantDate = (req, res, next) => {
   const { weekday } = req.body;
 
-  if (!weekday) {
+  if (weekday !== 0 && !weekday) {
     return res
       .status(httpStatusCodes.badRequest)
       .json({ error: 'Weekday is required' });
   }
 
-  const isValidWeekday = weekday >= 1 && weekday <= 5;
+  const isValidWeekday = weekday in weekdays;
 
   if (!isValidWeekday) {
     return res
