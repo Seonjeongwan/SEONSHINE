5803f0418334518c25e2e1d2601204963b2342f8
thoaiIT <nvthoai2k1@gmail.com>
2024-07-12 15:16:33 +0700

    implement get current profile api

--- seonshine_backend/controllers/commonController.js
+++ seonshine_backend/controllers/commonController.js
@@ -1,18 +1,19 @@
-import dayjs from "dayjs";
-import { Op, Sequelize } from "sequelize";
-import { UserRole, UserStatus } from "../constants/auth.js";
-import { dateTimeFormat } from "../constants/format.js";
-import { httpStatusCodes, httpStatusErrors } from "../constants/http.js";
-import { orderItemCancelStatus } from "../constants/order.js";
-import Branch from "../models/branchModel.js";
-import OrderItem from "../models/orderItemModel.js";
-import RestaurantAssigned from "../models/restaurantAssignedModel.js";
-import User from "../models/userModel.js";
-import { getResponseErrors } from "../utils/responseParser.js";
+import dayjs from 'dayjs';
+import { Op, Sequelize } from 'sequelize';
+import { UserRole, UserStatus } from '../constants/auth.js';
+import { dateTimeFormat } from '../constants/format.js';
+import { httpStatusCodes, httpStatusErrors } from '../constants/http.js';
+import { orderItemCancelStatus } from '../constants/order.js';
+import Branch from '../models/branchModel.js';
+import OrderItem from '../models/orderItemModel.js';
+import RestaurantAssigned from '../models/restaurantAssignedModel.js';
+import User from '../models/userModel.js';
+import { getResponseErrors } from '../utils/responseParser.js';
+import UserProfile from '../models/userProfileModel.js';
 
 export const getAllBranch = async (req, res) => {
   const branches = await Branch.findAll({
-    attributes: { exclude: ["created_at", "updated_at"] },
+    attributes: { exclude: ['created_at', 'updated_at'] },
   });
   res.status(httpStatusCodes.success).send(branches);
 };
@@ -25,7 +26,7 @@ export const getBranchById = async (req, res) => {
     } else {
       res
         .status(httpStatusCodes.badRequest)
-        .json({ error: "Branch not found" });
+        .json({ error: 'Branch not found' });
     }
   } catch (error) {
     res
@@ -68,7 +69,7 @@ export const getDashboardSummary = async (req, res) => {
 
 const getUserDashboardSummary = async (currentUserId) => {
   let response = {
-    today_restaurant_name: "",
+    today_restaurant_name: '',
     current_order_status: 0,
     current_order_item_name: 0,
     current_order_item_id: 0,
@@ -76,7 +77,7 @@ const getUserDashboardSummary = async (currentUserId) => {
   };
   const currentWeekDay = dayjs().day();
   const restaurantAssign = await RestaurantAssigned.findOne({
-    attributes: ["restaurant_id"],
+    attributes: ['restaurant_id'],
     where: {
       weekday: currentWeekDay,
     },
@@ -87,7 +88,7 @@ const getUserDashboardSummary = async (currentUserId) => {
 
   if (todayRestaurantId) {
     const restaurant = await User.findByPk(todayRestaurantId, {
-      attributes: ["username"],
+      attributes: ['username'],
     });
     response.today_restaurant_name = restaurant?.username;
   }
@@ -115,7 +116,7 @@ const getUserDashboardSummary = async (currentUserId) => {
   response.today_order_users_count = orderUsersCount;
 
   const currentOrderItem = await OrderItem.findOne({
-    attributes: ["item_id", "item_name"],
+    attributes: ['item_id', 'item_name'],
     where: {
       user_id: currentUserId,
       order_date: currentDateByFormat,
@@ -141,7 +142,7 @@ const getRestaurantDashboardSummary = async (restaurantId) => {
   };
 
   const restaurantAssigned = await RestaurantAssigned.findAll({
-    attributes: ["weekday"],
+    attributes: ['weekday'],
     where: {
       restaurant_id: restaurantId,
     },
@@ -154,15 +155,15 @@ const getRestaurantDashboardSummary = async (restaurantId) => {
 
 const getAdminDashboardSummary = async () => {
   let response = {
-    today_restaurant_id: "",
-    today_restaurant_name: "",
+    today_restaurant_id: '',
+    today_restaurant_name: '',
     ordered_users_count: 0,
     active_users_count: 0,
     waiting_approval_users_count: 0,
   };
   const currentWeekDay = dayjs().day();
   const restaurantAssign = await RestaurantAssigned.findOne({
-    attributes: ["restaurant_id"],
+    attributes: ['restaurant_id'],
     where: {
       weekday: currentWeekDay,
     },
@@ -173,7 +174,7 @@ const getAdminDashboardSummary = async () => {
 
   if (todayRestaurantId) {
     const restaurant = await User.findByPk(todayRestaurantId, {
-      attributes: ["username"],
+      attributes: ['username'],
     });
     response.today_restaurant_id = todayRestaurantId;
     response.today_restaurant_name = restaurant?.username;
@@ -181,8 +182,8 @@ const getAdminDashboardSummary = async () => {
 
   const userSummaryByStatus = await User.findAll({
     attributes: [
-      "user_status",
-      [Sequelize.fn("COUNT", Sequelize.col("user_status")), "count"],
+      'user_status',
+      [Sequelize.fn('COUNT', Sequelize.col('user_status')), 'count'],
     ],
     where: {
       role_id: {
@@ -192,7 +193,7 @@ const getAdminDashboardSummary = async () => {
         [Op.in]: [UserStatus.active, UserStatus.waitingConfirm],
       },
     },
-    group: ["user_status"],
+    group: ['user_status'],
     raw: true,
   });
 
@@ -229,4 +230,75 @@ const getAdminDashboardSummary = async () => {
   response.ordered_users_count = orderUsersCount;
 
   return response;
+};
+
+export const getCurrentProfile = async (req, res) => {
+  try {
+    const { user_id, role_id } = req.user;
+    const numberRoleId = Number(role_id);
+
+    const userAttributes = [
+      'user_id',
+      'role_id',
+      'username',
+      'email',
+      'phone_number',
+      'user_status',
+    ];
+
+    const profileAttributes =
+      numberRoleId === UserRole.restaurant
+        ? ['address', 'profile_picture_url']
+        : ['birth_date', 'address', 'profile_picture_url'];
+
+    const userProfile = await User.findOne({
+      where: { user_id },
+      attributes:
+        numberRoleId === UserRole.restaurant
+          ? userAttributes
+          : [...userAttributes, 'branch_id'],
+      include: {
+        model: UserProfile,
+        as: 'profile',
+        attributes: profileAttributes,
+      },
+      raw: true,
+      nest: true,
+    });
+
+    if (!userProfile) {
+      return res.status(httpStatusCodes.BAD_REQUEST).json({
+        message:
+          numberRoleId === UserRole.restaurant
+            ? 'Restaurant not found'
+            : 'User not found',
+      });
+    }
+
+    const profileData = { ...userProfile, ...userProfile.profile };
+    delete profileData.profile;
+
+    if (numberRoleId === UserRole.restaurant) {
+      const restaurantAssigned = await RestaurantAssigned.findOne({
+        attributes: ['weekday'],
+        where: { restaurant_id: user_id },
+        raw: true,
+      });
+      profileData.weekday = restaurantAssigned?.weekday || null;
+    } else {
+      const branchId = userProfile.branch_id;
+      if (branchId) {
+        const branch = await Branch.findByPk(branchId, { raw: true });
+        profileData.branch_name = branch?.branch_name || null;
+      } else {
+        profileData.branch_name = null;
+      }
+    }
+
+    res.status(httpStatusCodes.success).json(profileData);
+  } catch (error) {
+    res
+      .status(httpStatusCodes.internalServerError)
+      .json({ error: httpStatusErrors.internalServerError });
+  }
 };
