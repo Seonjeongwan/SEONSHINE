8d3838fee37db0734540b440701dcfa1d7c85260
sgo1026781 <phulocnguyen1610@gmail.com>
2024-07-15 17:32:28 +0700

    Sign Up Validation

---
+++ seonshine_backend/models/signUpModel.js
@@ -0,0 +1,99 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelizeUserDb } from "../db/dbConfig.js";
+
+class SignUp extends Model {}
+
+SignUp.init(
+  {
+    role_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Role ID is required",
+        },
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Username is required",
+        },
+        len: {
+          args: [8, 255],
+          msg: "Username must be between 8 and 255 characters long",
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Email is required",
+        },
+        isEmail: {
+          msg: "Invalid email address",
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Password is required",
+        },
+        len: {
+          args: [6, 255],
+          msg: "Password must be between 6 and 255 characters long",
+        },
+      },
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Phone number is required",
+        },
+        isNumeric: {
+          msg: "Phone number must contain only numbers",
+        },
+        len: {
+          args: [10, 10],
+          msg: "Phone number must be exactly 10 digits long",
+        },
+      },
+    },
+    branch_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isRequiredForRoleId1(value) {
+          if (this.role_id === "1" && !value) {
+            throw new Error("Branch is required");
+          }
+        },
+      },
+    },
+    address: {
+      type: DataTypes.STRING,
+      validate: {
+        isRequiredForRoleId2(value) {
+          if (this.role_id === "2" && !value) {
+            throw new Error("Address is required");
+          }
+        },
+      },
+    },
+  },
+  {
+    sequelize: sequelizeUserDb,
+    modelName: "",
+  }
+);
+
+export default SignUp;
