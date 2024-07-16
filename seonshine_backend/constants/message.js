4725e753163fb3a4a97db2d24d9c15cf88029aca
sgo1026781 <phulocnguyen1610@gmail.com>
2024-07-16 08:35:05 +0700

    delete message

--- seonshine_backend/constants/message.js
+++ seonshine_backend/constants/message.js
@@ -14,19 +14,3 @@ export const messageErrors = {
     "The verification code has expired. Please re-send the verification code to try again",
   incorrectIdPassword: "Incorrect id or password",
 };
-
-export const validationSignUpMessages = {
-  roleIdRequired: "Role ID is required",
-  usernameRequired: "Username is required",
-  usernameMinLength: "Username must be at least 3 characters long",
-  emailRequired: "Email is required",
-  emailInvalid: "Invalid email address",
-  passwordRequired: "Password is required",
-  passwordMinLength: "Password must be at least 6 characters long",
-  confirmPasswordMismatch: "Password confirmation does not match password",
-  fullNameRequired: "Full name is required",
-  phoneNumberRequired: "Phone number is required",
-  phoneNumberInvalid: "Invalid phone number",
-  branchIdRequired: "Branch ID is required",
-  addressRequired: "Address is required for role ID 1",
-};
