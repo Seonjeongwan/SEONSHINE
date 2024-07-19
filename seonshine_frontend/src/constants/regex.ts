4c8774b5edf4941e90372f31d5fc5e411f0a642a
thoaiIT <nvthoai2k1@gmail.com>
2024-07-19 10:52:56 +0700

    Implement Order Period Setting

--- seonshine_frontend/src/constants/regex.ts
+++ seonshine_frontend/src/constants/regex.ts
@@ -3,3 +3,4 @@ export const passwordRegex: RegExp = /^[a-zA-Z0-9!@#$_]{8,50}$/;
 export const otpRegex: RegExp = /^\d{6}$/;
 export const digitRegex: RegExp = /^\d+$/;
 export const phoneNumberRegex: RegExp = /^\d{10,15}$/;
+export const validTimeFormatRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
--- seonshine_frontend/src/pages/restaurantAssignment/index.tsx
+++ seonshine_frontend/src/pages/restaurantAssignment/index.tsx
@@ -1,17 +1,29 @@
 import { useEffect, useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
-import { Box } from '@mui/material';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
 
+import TimePicker from '@/components/molecules/timePicker';
 import Table from '@/components/organims/table';
 
-import { useGetOrderPeriodApi } from '@/apis/hooks/orderListApi.hook';
+import { useDeviceType } from '@/hooks/useDeviceType';
+
+import { useGetOrderPeriodApi, useSetOrderPeriodApi } from '@/apis/hooks/orderListApi.hook';
 import { useGetRestaurantAssignListApi, useRestaurantAssignApi } from '@/apis/hooks/restaurantAssignApi.hook';
 import { useGetAllRestaurantsApi } from '@/apis/hooks/userApi.hook';
 
 import { AssignTableHeader } from './components/AssignTableHeader';
+import { orderPeriodSchema, OrderPeriodSchemaType } from './schema';
 import { AssignTableType, SelectionsType } from './types';
+
+const convertToTimeString = (hour: number, minute: number): string => {
+  const formattedHour = hour.toString().padStart(2, '0');
+  const formattedMinute = minute.toString().padStart(2, '0');
+  return `${formattedHour}:${formattedMinute}`;
+};
 
 const RestaurantAssignment = () => {
   const [selections, setSelections] = useState<SelectionsType>({});
@@ -21,7 +33,26 @@ const RestaurantAssignment = () => {
   const { mutate: assignRestaurant } = useRestaurantAssignApi();
   const { data: orderPeriod } = useGetOrderPeriodApi();
 
+  const { mutate: setOrderPeriod, isPending } = useSetOrderPeriodApi();
+
   const queryClient = useQueryClient();
+
+  const startTime = convertToTimeString(Number(orderPeriod?.data.start_hour), Number(orderPeriod?.data.start_minute));
+  const endTime = convertToTimeString(Number(orderPeriod?.data.end_hour), Number(orderPeriod?.data.end_minute));
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm<OrderPeriodSchemaType>({
+    resolver: zodResolver(orderPeriodSchema),
+    values: {
+      startTime: startTime,
+      endTime: endTime,
+    },
+  });
+
+  const { isMobile } = useDeviceType();
 
   const getDataAssignTable: AssignTableType[] = useMemo(
     () =>
@@ -56,18 +87,36 @@ const RestaurantAssignment = () => {
     if (!orderPeriod) return false;
     const currentDayIndex = new Date().getDay();
     if (id !== currentDayIndex) return false;
-    const { startHour, startMinute, endHour, endMinute } = orderPeriod;
+    const { start_hour, start_minute, end_hour, end_minute } = orderPeriod.data;
 
     const now = new Date();
     const currentHour = now.getHours();
     const currentMinute = now.getMinutes();
     return (
-      (currentHour > startHour || (currentHour === startHour && currentMinute >= startMinute)) &&
-      (currentHour < endHour || (currentHour === endHour && currentMinute <= endMinute))
+      (currentHour > start_hour || (currentHour === start_hour && currentMinute >= start_minute)) &&
+      (currentHour < end_hour || (currentHour === end_hour && currentMinute <= end_minute))
     );
   };
 
   const columns = AssignTableHeader({ handleSelectChange, isSelectDisabled });
+
+  const onSubmitForm = (data: OrderPeriodSchemaType) => {
+    setOrderPeriod(
+      {
+        start: data.startTime,
+        end: data.endTime,
+      },
+      {
+        onSuccess: (res) => {
+          toast.success(res.message);
+          queryClient.invalidateQueries({ queryKey: ['getOrderPeriod'] });
+        },
+        onError: () => {
+          toast.success('Set order period failed.');
+        },
+      },
+    );
+  };
 
   useEffect(() => {
     if (assignedRestaurantList && allRestaurants) {
@@ -85,7 +134,7 @@ const RestaurantAssignment = () => {
   }, [assignedRestaurantList, allRestaurants]);
 
   return (
-    <Box className="px-4 md:px-8 mt-8 w-full lg:w-280">
+    <Box className="px-4 md:px-8 my-4 w-full lg:w-280">
       <Table<AssignTableType>
         data={!assignedRestaurantList ? [] : getDataAssignTable}
         columns={columns}
@@ -93,6 +142,52 @@ const RestaurantAssignment = () => {
         currentPage={0}
         onPageChange={() => {}}
       />
+      <Stack
+        direction="column"
+        gap={2}
+        className="mt-8"
+      >
+        <Typography
+          fontSize={20}
+          className="font-bold"
+        >
+          Available ordering time setting
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmitForm)}>
+          <Stack
+            className="bg-white p-8 rounded-md"
+            alignItems={isMobile ? 'flex-start' : 'center'}
+            direction={isMobile ? 'column' : 'row'}
+            gap={isMobile ? 6 : 8}
+          >
+            <Typography className="text-lg font-normal">Order starts every day from</Typography>
+            <Stack
+              alignItems="center"
+              gap={4}
+            >
+              <TimePicker
+                name="startTime"
+                control={control}
+              />
+              <Typography className="text-lg font-normal">to</Typography>
+              <TimePicker
+                name="endTime"
+                control={control}
+              />
+            </Stack>
+
+            <Button
+              variant="outlined"
+              color="primary"
+              type="submit"
+              disabled={!isDirty || isPending}
+              className="font-bold text-black-500 rounded-full bg-black-200 hover:bg-black-300 hover:text-white border-none hover:border-none h-auto py-2 px-10"
+            >
+              Save
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
     </Box>
   );
 };
