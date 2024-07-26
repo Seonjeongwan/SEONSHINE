f45fc0033a9db3f822600118795b06433ba1afad
thoaiIT <nvthoai2k1@gmail.com>
2024-07-26 15:03:07 +0700

    fix regex and optional chaining

--- seonshine_frontend/src/constants/regex.ts
+++ seonshine_frontend/src/constants/regex.ts
@@ -1,6 +1,6 @@
 import { errorMessages } from './errorMessages';
 
-export const employeeIdRegex: RegExp = /^[a-zA-Z0-9]{4,20}$/;
+export const employeeIdRegex: RegExp = /^[a-zA-Z0-9]{8,20}$/;
 export const passwordRegex: RegExp = /^[a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\\]{8,50}$/;
 export const otpRegex: RegExp = /^\d{6}$/;
 export const digitRegex: RegExp = /^\d+$/;
